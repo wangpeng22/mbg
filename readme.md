7b69d4b2af4f81e81deecde13772ad05973eeb6a
wangpeng <123456789>
2018-12-18 16:19:04 +0800

    init

---
+++ readme.md
@@ -0,0 +1,9 @@
+MyBatis Generator
+
+1.在pom.xml增加自动生成mapper的插件
+
+2.修改配置generatorConfig.xml文件
+
+3.运行:
+在idea中新建一个maven Run/Debug configurations 
+command line : mybatis-generator:generate -e
---
+++ src/main/java/club/sanchi/mbg/MbgApplication.java
@@ -0,0 +1,14 @@
+package club.sanchi.mbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbgApplication.class, args);
+	}
+
+}
+
